c94f92950a30615df3b750c68f8a29eb88c6527f
Haeryz00 <muh4mm4dh4r1z@gmail.com>
2023-05-01 21:55:08 +0700

    Fisrt commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
 }
 
 class TopUpScreen extends StatefulWidget {
-  const TopUpScreen({super.key});
+  const TopUpScreen({Key? key}) : super(key: key);
 
   @override
   // ignore: library_private_types_in_public_api
@@ -24,6 +24,25 @@ class TopUpScreen extends StatefulWidget {
 
 class _TopUpScreenState extends State<TopUpScreen> {
   String _resultMessage = '';
+  String _selectedGame = 'Free Fire';
+  final List<Map<String, dynamic>> _gameList = [
+    {
+      'name': 'Free Fire',
+      'image': const AssetImage('image/FF.jpg'),
+    },
+    {
+      'name': 'PUBG Mobile',
+      'image': const AssetImage('image/papji.jpg'),
+    },
+    {
+      'name': 'Mobile Legends',
+      'image': const AssetImage('image/ML.jpg'),
+    },
+    {
+      'name': 'Call of Duty: Mobile',
+      'image': const AssetImage('image/cod.jpg'),
+    },
+  ];
 
   void _processTopUpRequest() {
     // Add logic to process the top-up request here
@@ -44,13 +63,57 @@ class _TopUpScreenState extends State<TopUpScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Enter top-up amount',
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1.5,
               ),
-              onChanged: (value) {
-                setState(() {});
+              itemCount: _gameList.length,
+              itemBuilder: (BuildContext context, int index) {
+                final game = _gameList[index];
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _selectedGame = game['name'];
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      border: Border.all(
+                        color: game['name'] == _selectedGame
+                            ? Colors.green
+                            : Colors.grey,
+                        width: 2.0,
+                      ),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image(
+                            image: game['image'],
+                            height: 64.0,
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            game['name'],
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0,
+                              color: game['name'] == _selectedGame
+                                  ? Colors.green
+                                  : Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
               },
             ),
             const SizedBox(height: 16.0),
